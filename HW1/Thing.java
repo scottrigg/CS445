f81ad1977c7607d63d74d1d4c63ab3e797c25051
scottrigg <srigg@hawk.iit.edu>
2016-02-09 12:56:24 -0600

    Create Thing.java

---
+++ HW1/Thing.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ * @author Scott Rigg 
+ * 2/9/16
+ *
+ */
+public class Thing 
+{
+	String name;
+	
+	public Thing(String name)
+	{
+		this.name=name;
+	}
+	
+	@Override
+	public String toString()
+	{
+		String cName = getClass().getSimpleName();
+		return this.name + " " + cName;
+	}
+	
+}
