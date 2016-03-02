921d74a2047ff560d2b3496eb1a1eb7816c11342
scottrigg <srigg@hawk.iit.edu>
2016-03-02 16:37:28 -0600

    Create Lightbulb.java
    Created the lightbulb class

---
+++ HW2/src/lightbulb/Lightbulb.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ * @author Scott Rigg
+ *
+ */
+package lightbulb;
+public class Lightbulb {
+
+	public void on()
+	{
+		System.out.println("Lightbulb on");
+	}
+	
+	public void off()
+	{
+		System.out.println("Lightbulb off");
+	}
+}
