0623d06681746db2e60b576a0e99e60285480598
scottrigg <srigg@hawk.iit.edu>
2016-04-17 15:00:51 -0500

    Create Menu.java

---
+++ delectable/srigg/Delectable/objects/Menu.java
@@ -0,0 +1,46 @@
+package srigg.Delectable.objects;
+
+import java.util.ArrayList;
+
+public class Menu {
+
+	private double surcharge=0;
+	private ArrayList<Food> menulisting;
+	
+	
+	private Menu()
+	{
+		menulisting = new ArrayList<Food>();
+	}
+	
+	public ArrayList<Food> menuListing()
+	{
+		return menulisting;
+	}
+	
+
+	public void deleteFoodItem(int fid)
+	{
+		for(int i=0; i<menulisting.size();i++)
+		{
+			Food f = menulisting.get(i);
+			if(f.getFID()==fid)
+			{
+				menulisting.remove(f);
+			}
+			
+		}
+	}
+	
+	public void addFoodItem(Food food1)
+	{
+		menulisting.add(food1);
+	}
+
+//	public boolean matchesId(int fid) {
+//		// TODO Auto-generated method stub
+//		
+//		return fid==OID;
+//		
+//	}
+}
